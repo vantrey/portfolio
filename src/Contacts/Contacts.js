83ca16d230ed24b3ee9025db11b5bd3e5f4c4bd4
Ivan Tolkachev <vantrey@yandex.ru>
2020-06-18 14:00:54 +0300

    add isFetching

--- src/Contacts/Contacts.js
+++ src/Contacts/Contacts.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from './Contacts.module.css'
 import Button from "../Button/Button"
 import Title from "../Tilte/Title"
@@ -14,30 +14,53 @@ const Contacts = () => {
     email: yup.string().required('⚠ please, fill up your email')
       .email('⚠ please, fill up a valid email address'),
     message: yup.string().required('⚠ please, enter your message')
-  })
+  });
+  const [isFetching, setIsFetching] = useState(false)
+  const {register, handleSubmit, errors, reset} = useForm({mode: 'onBlur', validationSchema: sendMessageSchema});
 
-  const {register, handleSubmit, errors} = useForm({mode: 'onBlur', validationSchema: sendMessageSchema});
-  const onSubmit = data => {
-    console.log(data)
-    axios.post('https://nodejs-smtp-server.herokuapp.com/sendMessage', {
-      data
-    }).then(()=>{
-      alert('your message has benn sent')
-    })
+  const onSubmit = async data => {
+
+    reset()
+
+    try {
+      setIsFetching(true)
+      await axios.post(
+        'https://nodejs-smtp-server.herokuapp.com/sendMessage', {
+          data
+        })
+      setIsFetching(false)
+
+    } catch (e) {
+      console.log({...e})
+      setIsFetching(false)
+    }
   }
+
   return (
     <div id={'contacts'} className={styles.contacts}>
+
       <div className={styles.container}>
+
         <div>
           <Title isSmall={false}>contact</Title>
         </div>
+
         <Fade top>
+
           <form onSubmit={handleSubmit(onSubmit)} className={styles.contactsForm}>
+
             <TextField type={`input`} name={`name`} register={register} errorMessage={errors.name}/>
             <TextField type={`input`} name={'email'} register={register} errorMessage={errors.email}/>
             <TextField type={`textarea`} name={`message`} register={register} errorMessage={errors.message}/>
-            <div className={styles.contactsButton}><Button buttonName={`send message`} buttonLink={``}/></div>
+            <div className={styles.contactsButton}>
+
+              {(isFetching && <div>...sending</div>) ||
+
+              <Button buttonName={`send message`} buttonLink={``}/>}
+            </div>
+
           </form>
+
         </Fade>
       </div>
     </div>
